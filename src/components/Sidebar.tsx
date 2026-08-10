b30a962a587c057f7ca04f186816c9f62e00acc3
johnnysmilist-hue <johnnysmilist@gmail.com>
2026-08-10 12:15:50 +0300

    Update sidebar links to include category parameters

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -18,9 +18,14 @@ export default function Sidebar() {
     <aside className="w-full md:w-56 shrink-0">
       <div className="border border-gray-200 dark:border-gray-800 bg-white dark:bg-gray-900 rounded-lg p-4">
         <ul className="text-sm divide-y divide-gray-100 dark:divide-gray-800">
+          <li>
+            <a href="/shop" className="block py-2 text-black dark:text-white hover:text-brand font-semibold">
+              All Products
+            </a>
+          </li>
           {categories.map((cat) => (
             <li key={cat}>
-              <a href="/shop" className="block py-2 text-black dark:text-white hover:text-brand">
+              <a href={"/shop?category=" + encodeURIComponent(cat)} className="block py-2 text-black dark:text-white hover:text-brand">
                 {cat}
               </a>
             </li>
