cff0669e25bdfc60d8e9d18cde93b37df345bb73
johnnysmilist-hue <johnnysmilist@gmail.com>
2026-08-10 10:11:53 +0300

    Add TopPicks component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Categories from "@/components/Categories";
+import TopPicks from "@/components/TopPicks";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Hero />
       <Categories />
+      <TopPicks />
     </main>
   );
 }
