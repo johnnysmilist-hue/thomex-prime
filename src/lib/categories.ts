3e14618a2798df0f935ee7fc20142befe9a490c3
johnnysmilist-hue <johnnysmilist@gmail.com>
2026-08-12 15:20:51 +0300

    Add categories array for product classification

---
+++ src/lib/categories.ts
@@ -0,0 +1,14 @@
+export const categories = [
+  "Laptops",
+  "PC & Computers",
+  "Accessories",
+  "Gaming & VR",
+  "Networking",
+  "Office",
+  "Sounds",
+  "Cameras",
+  "Cell Phones",
+  "Tablets",
+  "Storage, USB",
+  "Clearance",
+];
