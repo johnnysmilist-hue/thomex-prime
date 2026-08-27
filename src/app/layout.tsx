66228a30cab6431c2147884e1b59a544c252237f
johnnysmilist-hue <johnnysmilist@gmail.com>
2026-08-27 15:53:54 +0300

    Add ChatWidget component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
                   <RecentlyViewedProvider>
                     {children}
                     <MobileNav />
+                    <ChatWidget />
                   </RecentlyViewedProvider>
                 </WishlistProvider>
               </CartProvider>
