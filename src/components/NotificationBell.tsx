420d34b18e3838dc71a60a58c8f4b2fbd814b689
johnnysmilist-hue <johnnysmilist@gmail.com>
2026-09-02 10:10:28 +0300

    Add flex styling to NotificationBell button

--- src/components/NotificationBell.tsx
+++ src/components/NotificationBell.tsx
@@ -89,7 +89,7 @@ export default function NotificationBell() {
 
   return (
     <div className="relative">
-      <button onClick={handleOpen} aria-label="Notifications" className="relative">
+       <button onClick={handleOpen} aria-label="Notifications" className="relative flex items-center justify-center w-full h-full">
          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-gray-500 dark:text-gray-400">
           <path d="M18 8a6 6 0 0 0-12 0c0 7-3 9-3 9h18s-3-2-3-9" />
           <path d="M13.73 21a2 2 0 0 1-3.46 0" />
